83f63ffbb9ad907db9234dd7e61d3bf10388c931
Dahabu Saidi <dahabusaidi@gmail.com>
2022-04-05 16:55:36 +0300

    Change menu icons and names

--- src/App.tsx
+++ src/App.tsx
@@ -9,9 +9,9 @@ function App() {
     <nav aria-label="menu nav" className="bg-gray-600 pt-2 md:pt-1 pb-1 px-1 mt-0 h-auto fixed w-full z-20 top-0">
 
         <div className="flex flex-wrap items-center">
-            <div className="flex flex-shrink md:w-1/3 justify-center md:justify-start text-white">
+            <div className="flex flex-shrink md:w-1/3 justify-center md:justify-start text-white pl-3">
                 <a href="#" aria-label="Home">
-                    <span className="text-xl pl-2"><i className="em em-grinning"></i></span>
+                    React Admin Skeleton
                 </a>
             </div>
 
@@ -24,10 +24,16 @@ function App() {
             <div className="flex w-full pt-2 content-center justify-between md:w-1/3 md:justify-end">
                 <ul className="list-reset flex justify-between flex-1 md:flex-none items-center">
                     <li className="flex-1 md:flex-none md:mr-3">
-                        <a className="inline-block py-2 px-4 text-white no-underline" href="#">Active</a>
+                        <a className="inline-block py-2 px-4 text-white no-underline" href="#">Home</a>
                     </li>
                     <li className="flex-1 md:flex-none md:mr-3">
-                        <a className="inline-block text-gray-400 no-underline hover:text-gray-200 hover:text-underline py-2 px-4" href="#">link</a>
+                        <a className="inline-block text-gray-400 no-underline hover:text-gray-200 hover:text-underline py-2 px-4" href="#">Users</a>
+                    </li>
+                    <li className="flex-1 md:flex-none md:mr-3">
+                        <a className="inline-block text-gray-400 no-underline hover:text-gray-200 hover:text-underline py-2 px-4" href="#">Products</a>
+                    </li>
+                    <li className="flex-1 md:flex-none md:mr-3">
+                        <a className="inline-block text-gray-400 no-underline hover:text-gray-200 hover:text-underline py-2 px-4" href="#">Orders</a>
                     </li>
                     <li className="flex-1 md:flex-none md:mr-3">
                         
@@ -48,22 +54,28 @@ function App() {
                     <ul className="list-reset flex flex-row md:flex-col pt-3 md:py-3 px-1 md:px-2 text-center md:text-left">
                         <li className="mr-3 flex-1">
                             <a href="#" className="block py-1 md:py-3 pl-1 align-middle text-white no-underline hover:text-white border-b-2 border-gray-800 hover:border-pink-500">
-                                <i className="fas fa-tasks pr-0 md:pr-3"></i><span className="pb-1 md:pb-0 text-xs md:text-base text-gray-400 md:text-gray-200 block md:inline-block">Tasks</span>
+                                <i className="fas fa-tasks pr-0 md:pr-3"></i><span className="pb-1 md:pb-0 text-xs md:text-base text-gray-400 md:text-gray-200 block md:inline-block">Dashboard</span>
                             </a>
                         </li>
                         <li className="mr-3 flex-1">
                             <a href="#" className="block py-1 md:py-3 pl-1 align-middle text-white no-underline hover:text-white border-b-2 border-gray-800 hover:border-purple-500">
-                                <i className="fa fa-envelope pr-0 md:pr-3"></i><span className="pb-1 md:pb-0 text-xs md:text-base text-gray-400 md:text-gray-200 block md:inline-block">Messages</span>
+                                <i className="fa fa-users pr-0 md:pr-3"></i><span className="pb-1 md:pb-0 text-xs md:text-base text-gray-400 md:text-gray-200 block md:inline-block">User Management</span>
                             </a>
                         </li>
                         <li className="mr-3 flex-1">
                             <a href="#" className="block py-1 md:py-3 pl-1 align-middle text-white no-underline hover:text-white border-b-2 border-blue-600">
-                                <i className="fas fa-chart-area pr-0 md:pr-3 text-blue-600"></i><span className="pb-1 md:pb-0 text-xs md:text-base text-white md:text-white block md:inline-block">Analytics</span>
+                                <i className="fa fa-user-shield pr-0 md:pr-3 text-blue-600"></i><span className="pb-1 md:pb-0 text-xs md:text-base text-white md:text-white block md:inline-block">Roles & Permissions</span>
                             </a>
                         </li>
                         <li className="mr-3 flex-1">
                             <a href="#" className="block py-1 md:py-3 pl-0 md:pl-1 align-middle text-white no-underline hover:text-white border-b-2 border-gray-800 hover:border-red-500">
-                                <i className="fa fa-wallet pr-0 md:pr-3"></i><span className="pb-1 md:pb-0 text-xs md:text-base text-gray-400 md:text-gray-200 block md:inline-block">Payments</span>
+                                <i className="fa fa-list pr-0 md:pr-3"></i><span className="pb-1 md:pb-0 text-xs md:text-base text-gray-400 md:text-gray-200 block md:inline-block">Products</span>
+                            </a>
+                        </li>
+
+                        <li className="mr-3 flex-1">
+                            <a href="#" className="block py-1 md:py-3 pl-0 md:pl-1 align-middle text-white no-underline hover:text-white border-b-2 border-gray-800 hover:border-red-500">
+                                <i className="fa fa-shopping-cart pr-0 md:pr-3"></i><span className="pb-1 md:pb-0 text-xs md:text-base text-gray-400 md:text-gray-200 block md:inline-block">Orders</span>
                             </a>
                         </li>
                     </ul>
@@ -74,9 +86,9 @@ function App() {
         </nav>
         <section>
             <div id="main" className="main-content flex-1 bg-white mt-12 md:mt-2 pb-24 p-10 md:pb-5">
-                <div className="flex flex-row flex-wrap flex-grow mt-2">
+                <div className="flex flex-row flex-wrap flex-grow mt-8">
 
-                    fasdfasf
+                    Dashboard
                 </div>
             </div>
         </section>
