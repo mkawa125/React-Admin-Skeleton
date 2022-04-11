80b47d01edeeec6b404e86782632b1fdba6ff7bb
Dahabu Saidi <dahabusaidi@gmail.com>
2022-04-11 17:29:20 +0300

    Remove unused resources

--- src/modules/users/Users.tsx
+++ src/modules/users/Users.tsx
@@ -1,8 +1,4 @@
 import React, { Component } from "react";
-import Team1 from 'assets/img/team-1-800x800.jpg';
-import Team2 from 'assets/img/team-2-800x800.jpg';
-import Team3 from 'assets/img/team-3-800x800.jpg';
-import Team4 from 'assets/img/team-4-470x470.png';
 
 class User extends Component {
     render() {
