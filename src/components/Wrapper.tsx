50dc11b5ce26dbbad5bfeede7e65852fd2f3882f
Dahabu Saidi <dahabusaidi@gmail.com>
2022-05-03 08:36:16 +0300

    Redirect to login id user unauthenticated

--- src/components/Wrapper.tsx
+++ src/components/Wrapper.tsx
@@ -1,20 +1,37 @@
-import React, { Component } from "react";
+import React, { Component, useEffect, useState } from "react";
 import  SideNavBar  from "./SideBar";
 import Nav from "./Nav";
+import axios from "axios";
+import  {Navigate}  from "react-router-dom";
 
-class Wrapper extends Component{
+const Wrapper  = (props: any) => {
 
-    render(){
+    const [redirect, setRedirect] = useState(false);
+    useEffect(() => {
+        (
+           async () => {
+               try {
+                const {data} = await axios.get('user');
+                console.log(data)
+               } catch (error) {
+                setRedirect(true)
+               }
+           }
+        )();
+    }, []);
+
+    if (redirect) {
+        return <Navigate to="/login"/>
+    }
         return (
             <>
                <SideNavBar/>
                 <Nav/>
                 <section className='pt-24 bg-gray-100 w-full p-20'>
-                    {this.props.children}
+                    {props.children}
                 </section>
             </>
         )
-    }
 }
 
 export default Wrapper
