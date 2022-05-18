ea6e38ae09d9c5619f43a18e04200b8c921bce65
root <root@localhost.localdomain>
2022-05-18 16:12:22 +0300

    Code refactor

--- src/modules/products/forms/createProduct.tsx
+++ src/modules/products/forms/createProduct.tsx
@@ -1,88 +1,76 @@
 import axios from "axios";
-import React, { Component, SyntheticEvent, useEffect, useState } from "react";
-import { Link } from "react-router-dom";
+import {SyntheticEvent, useState } from "react";
 import Wrapper from "../../../components/Wrapper";
-import { Permission } from "../../roles_and_permissions/permissionModel";
 
 
 const CreateProduct  = () => {
 
-    const [name, setRoleName] = useState('');
+    const [name, setName] = useState('');
+    const [description, setDescription] = useState('');
+    const [price, setPrice]= useState('');
+    const [image, setImage] = useState('https://images.unsplash.com/photo-1624555130581-1d9cca783bc0?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1171&q=80');
     const [redirect, setRedirect] = useState(false);
-    const [permissions, setPermissions] = useState([]);
-    const [selected, setSelected] = useState([] as number[]);
     
-    useEffect(() =>{
-        (
-           async () => {
-             const {data} = await axios.get('permissions');
-             setPermissions(data.data);
-           }
-        )()
-    }, [])
 
     const submit = async (e: SyntheticEvent) => {
         
         e.preventDefault();
 
-        const response = await axios.post("roles", {
+        const response = await axios.post("products/create", {
             name,
-            permissions: selected
+            description,
+            price,
+            image
         });
 
-        window.location.href = '/roles';
+        window.location.href = '/products';
         setRedirect(true);
     }
 
     /** This needs to understand and refactor */
 
-    const checked = (id:number) => {
-        if (selected.some(s => s === id)) {
-            setSelected(selected.filter(s => s !== id))
-            return
-        }
-        setSelected([...selected, id])
-    }
     return (
         <Wrapper>
             <div className="items-center mx-auto bg-white p-8 shadow  w-2/3  dark:bg-gray-900">
                 <div className="">
                 <form onSubmit={submit}>
                     <label className="block">
-                        <span className="text-gray-700 dark:text-gray-400">Role Name</span>
+                        <span className="text-gray-700 dark:text-gray-400">Product Name</span>
                         <input
                         className="block w-full py-2 px-2 border rounded mt-1 focus:outline-none  form-input"
-                        type="text" 
-                        onChange={e => setRoleName(e.target.value)}
+                        type="text"
+                        defaultValue={name}
+                        onChange={e => setName(e.target.value)}
                         required
-                        placeholder="Role Name"
+                        placeholder="Product Name"
                         />
                     </label>
 
-                    <label className="block mt-4 mb-2">
-                        <span className="text-gray-700 mt-4 dark:text-gray-400">Permissions</span>
+                    <label className="block mt-4">
+                        <span className="text-gray-700 dark:text-gray-400">Price</span>
+                        <input
+                        className="block w-full py-2 px-2 border rounded mt-1 focus:outline-none  form-input"
+                        type="number"
+                        defaultValue={price}
+                        onChange={e => setPrice(e.target.value)}
+                        required
+                        placeholder="Price"
+                        />
                     </label>
-                    <div className="grid grid-cols-4">
-                    {
-                        permissions.map((permission: Permission) => {
-                            return (
-                                <div>
-                                    <input
-                                    className=" py-2 mr-2 px-2 border rounded mt-1 focus:outline-none"
-                                    type="checkbox"
-                                    value={permission.id}
-                                    onChange={e => checked(permission.id)}/>
-                                
-                                    <label className="">
-                                        <span className="text-gray-700 dark:text-gray-400">{ permission.name }</span>
-                                    </label>
-                                </div>
-                            )
-                        })
-                    }
-                    </div>
+
+                    <label className="block mt-4">
+                        <span className="text-gray-700 dark:text-gray-400">Description</span>
+                    </label>
+                        <textarea
+                            className="block w-full py-2 px-2 border rounded mt-1 focus:outline-none  form-input"
+                            onChange={e => setDescription(e.target.value)}
+                            required
+                            rows={4}
+                            placeholder="Product descriptions"
+                        >
+                        </textarea>
                     
-                    <button className="block w-full px-4 py-2 mt-4 font-bold text-center text-white bg-gray-500 border border-transparent rounded active:bg-purple-600 hover:bg-gray-700 "
+                        <button className="block w-full px-4 py-2 mt-4 font-bold text-center text-white bg-gray-600 border border-transparent rounded active:bg-purple-600 hover:bg-gray-700 "
                         type="submit">
                         Save
                     </button>
