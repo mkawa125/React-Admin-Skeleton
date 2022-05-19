f58fceb01e9bfae6e30b3722b914fffa3e7fe0ee
root <root@localhost.localdomain>
2022-05-19 09:01:27 +0300

    Add font-awesome icons

--- src/components/SideBar.tsx
+++ src/components/SideBar.tsx
@@ -11,7 +11,7 @@ const SideBar = () => {
     return (
             <aside className="z-20 flex flex-col h-screen hidden w-64 overflow-y-auto bg-gray-300  border dark:bg-gray-800 md:block flex-shrink-0">
                 <div className="py-4 text-gray-900 dark:text-gray-400">
-                    <Link className="ml-6 text-lg font-bold text-gray-800 dark:text-gray-200" to={'/'}>
+                    <Link className="ml-6 text-lg font-bold text-gray-600 dark:text-gray-200" to={'/'}>
                         React Admin Skeleton
                     </Link>
 
@@ -19,10 +19,7 @@ const SideBar = () => {
                         <li className="relative px-6 py-3 bg-gray-400">
                             <span className="absolute inset-y-0 left-0 w-1 bg-blue-900 rounded-tr-lg rounded-br-lg" aria-hidden="true"></span>
                             <NavLink className="inline-flex items-center w-full text-sm font-semibold text-gray-800 transition-colors duration-150 hover:text-gray-800 dark:hover:text-gray-200 dark:text-gray-100" to={'/'}>
-                                <svg className="w-5 h-5" aria-hidden="true" fill="none" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" viewBox="0 0 24 24" stroke="currentColor">
-                                    <path d="M3 12l2-2m0 0l7-7 7 7M5 10v10a1 1 0 001 1h3m10-11l2 2m-2-2v10a1 1 0 01-1 1h-3m-6 0a1 1 0 001-1v-4a1 1 0 011-1h2a1 1 0 011 1v4a1 1 0 001 1m-6 0h6">
-                                    </path>
-                                </svg>
+                            <i className="fa fa-home"></i>
                                 <span className="ml-4">Dashboard</span>
                             </NavLink>
                         </li>
@@ -32,10 +29,7 @@ const SideBar = () => {
                     <ul>
                         <li className="relative px-6 py-3">
                             <NavLink className="inline-flex items-center w-full text-sm transition-colors duration-150 hover:text-gray-800 dark:hover:text-gray-200" to={'/users'}>
-                                <svg className="w-5 h-5" aria-hidden="true" fill="none" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" viewBox="0 0 24 24" stroke="currentColor">
-                                    <path d="M9 5H7a2 2 0 00-2 2v12a2 2 0 002 2h10a2 2 0 002-2V7a2 2 0 00-2-2h-2M9 5a2 2 0 002 2h2a2 2 0 002-2M9 5a2 2 0 012-2h2a2 2 0 012 2m-3 7h3m-3 4h3m-6-4h.01M9 16h.01">
-                                    </path>
-                                </svg>
+                                <i className="fa fa-users"></i> 
                                 <span className="ml-4">Users Management</span>
                             </NavLink>
                         </li>
@@ -53,10 +47,7 @@ const SideBar = () => {
 
                         <li className="relative px-6 py-3">
                             <NavLink to={"/roles"} className="inline-flex items-center w-full text-sm  transition-colors duration-150 hover:text-gray-800 dark:hover:text-gray-200">
-                            <svg className="w-5 h-5" aria-hidden="true" fill="none" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" viewBox="0 0 24 24" stroke="currentColor">
-                                    <path d="M9 5H7a2 2 0 00-2 2v12a2 2 0 002 2h10a2 2 0 002-2V7a2 2 0 00-2-2h-2M9 5a2 2 0 002 2h2a2 2 0 002-2M9 5a2 2 0 012-2h2a2 2 0 012 2m-3 7h3m-3 4h3m-6-4h.01M9 16h.01">
-                                    </path>
-                                </svg>
+                                <i className="fa fa-user-secret"></i>
                                 <span className="ml-4">Roles & Permissions</span>
                             </NavLink>
                         </li>
@@ -65,10 +56,7 @@ const SideBar = () => {
 
                         <li className="relative px-6 py-3">
                             <NavLink to={'/products'} className="inline-flex items-center w-full text-sm transition-colors duration-150 hover:text-gray-800 dark:hover:text-gray-200">
-                                <svg className="w-5 h-5" aria-hidden="true" fill="none" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" viewBox="0 0 24 24" stroke="currentColor">
-                                    <path d="M19 11H5m14 0a2 2 0 012 2v6a2 2 0 01-2 2H5a2 2 0 01-2-2v-6a2 2 0 012-2m14 0V9a2 2 0 00-2-2M5 11V9a2 2 0 012-2m0 0V5a2 2 0 012-2h6a2 2 0 012 2v2M7 7h10">
-                                    </path>
-                                </svg>
+                            <i className="fa fa-list"></i>
                                 <span className="ml-4">Products </span>
                             </NavLink>
                         </li>
@@ -106,9 +94,9 @@ const SideBar = () => {
                         </template>
                     </ul>
 
-                    <div className="px-6 my-6">
-                        <NavLink to={'/login'} onClick={logout}>
-                            <button className="flex items-center justify-between w-full px-4 py-2 text-sm font-medium leading-5 text-white transition-colors duration-150 bg-blue-900 border border-transparent rounded active:bg-blue-600 hover:bg-blue-700 focus:outline-none focus:shadow-outline-blue">
+                    <div className="px-6 my-6 items-center">
+                        <NavLink to={'/login'} onClick={logout} className="items-center">
+                            <button className="flex items-center text-center justify-between w-full px-4 py-2 text-sm font-medium leading-5 text-white transition-colors duration-150 bg-blue-900 border border-transparent rounded active:bg-blue-600 hover:bg-blue-700 focus:outline-none focus:shadow-outline-blue">
                                 Logout 
                             </button>
                         </NavLink>
