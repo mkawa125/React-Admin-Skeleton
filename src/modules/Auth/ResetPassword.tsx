291842f10015c1e3619be056e35bd729641a83f4
Dahabu Saidi <dahabusaidi@gmail.com>
2022-06-10 09:44:06 +0300

    Code refactor

--- src/modules/Auth/ResetPassword.tsx
+++ src/modules/Auth/ResetPassword.tsx
@@ -98,7 +98,6 @@ const ResetPassword  = () => {
                                     </p>                                
                                 {/* } */}
 
-                                
     
                                 <button className="block w-full px-4 py-2 mt-4 text-sm font-medium leading-5 text-center text-white transition-colors duration-150 bg-blue-800 border border-transparent rounded active:bg-purple-600 hover:bg-purple-700 focus:outline-none focus:shadow-outline-purple"
                                     type="submit">
